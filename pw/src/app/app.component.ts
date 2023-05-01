3ffebb477fe0361223e25a78f473cafa8efafb45
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-04-30 21:12:51 -0400

    first commit

---
+++ pw/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  includeLetters = false;
+  password = '';
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onButtonClick() {
+    this.password = 'MY PASSWORD!!!';
+  }
+
+}
